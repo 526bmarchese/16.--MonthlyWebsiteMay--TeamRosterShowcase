d8391dabee17205bfe657f174cb3fb049d6d98e2
Brandon Marchese <526bmarchese@frhsd.com>
2025-05-16 15:51:31 +0000

    Day #2: a

--- players.js
+++ players.js
@@ -1,38 +1,215 @@
 const players = [
     {
-        firstName: 'Francisco',
-        lastName: 'Lindor',
-        position: 'SS',
-        age: 31,
-        photo: 'https://picsum.photos/200/300'
+        firstName: 'Aaron',
+        lastName: 'Judge',
+        position: 'RF',
+        age: 33,
+        photo: 'imgs/judge.png'
     },
     {
-        firstName: 'Francisco',
-        lastName: 'Lindor',
-        position: 'SS',
-        age: 31,
-        photo: 'https://picsum.photos/200/300'
+        firstName: 'Cody',
+        lastName: 'Bellinger',
+        position: '1B',
+        age: 29,
+        photo: 'imgs/bellinger.png'
     },
     {
-        firstName: 'Francisco',
-        lastName: 'Lindor',
-        position: 'SS',
-        age: 31,
-        photo: 'https://picsum.photos/200/300'
+        firstName: 'Jasson',
+        lastName: 'Dominnguez',
+        position: 'LF',
+        age: 22,
+        photo: 'imgs/jasond.png'
     },
     {
-        firstName: 'Francisco',
-        lastName: 'Lindor',
-        position: 'SS',
-        age: 31,
-        photo: 'https://picsum.photos/200/300'
+        firstName: 'Trent',
+        lastName: 'Grisham',
+        position: 'CF',
+        age: 28,
+        photo: 'imgs/grisham.png'
     },
     {
-        firstName: 'Francisco',
-        lastName: 'Lindor',
+        firstName: 'Anthony',
+        lastName: 'Volpe',
+        position: 'SS',
+        age: 24,
+        photo: 'imgs/volpe.png'
+    },
+     {
+        firstName: 'Jorbit',
+        lastName: 'Vivas',
+        position: '2B',
+        age: 24,
+        photo: 'imgs/vivas.png'
+    },
+    {
+        firstName: 'Ben',
+        lastName: 'Rice',
+        position: 'DH',
+        age: 26,
+        photo: 'imgs/rice.png'
+    },
+    {
+        firstName: 'Pablo',
+        lastName: 'Reyes',
         position: 'SS',
         age: 31,
-        photo: 'https://picsum.photos/200/300'
-    }
+        photo: 'imgs/reyes.png'
+    },
+    {
+        firstName: 'Oswald',
+        lastName: 'Peraza',
+        position: '2B',
+        age: 24,
+        photo: 'imgs/peraza.png'
+    },
+     {
+        firstName: 'DJ',
+        lastName: 'LeMahieu',
+        position: '1B',
+        age: 36,
+        photo: 'imgs/dj.png'
+    },
+    {
+        firstName: 'Paul',
+        lastName: 'Goldschmidt',
+        position: '1B',
+        age: 37,
+        photo: 'imgs/goldshmidt.png'
+    },
+    {
+        firstName: 'Jazz',
+        lastName: 'Chisholm Jr.',
+        position: '2B',
+        age: 27,
+        photo: 'imgs/jazz.png'
+    },
+    {
+        firstName: 'Oswaldo',
+        lastName: 'Cabrera',
+        position: '3B',
+        age: 26,
+        photo: 'imgs/cabrera.png'
+    },
+     {
+        firstName: 'Austin',
+        lastName: 'Wells',
+        position: 'C',
+        age: 25,
+        photo: 'imgs/wells.png'
+    },
+    {
+        firstName: 'J.C.',
+        lastName: 'Escarra',
+        position: 'C',
+        age: 30,
+        photo: 'imgs/escarra.png'
+    },
+    {
+        firstName: 'Ryan',
+        lastName: 'Yarbrough',
+        position: 'RP',
+        age: 33,
+        photo: 'imgs/yarbrough.png'
+    },
+    {
+        firstName: 'Devin',
+        lastName: 'Williams',
+        position: 'RP',
+        age: 30,
+        photo: 'imgs/williams.png'
+    },
+     {
+        firstName: 'Luke',
+        lastName: 'Weaver',
+        position: 'RP',
+        age: 31,
+        photo: 'imgs/weaver.png'
+    },
+    {
+        firstName: 'Will',
+        lastName: 'Warren',
+        position: 'P',
+        age: 25,
+        photo: 'imgs/warren.png'
+    },
+    {
+        firstName: 'Marcus',
+        lastName: 'Stroman',
+        position: 'P',
+        age: 34,
+        photo: 'imgs/stroman.png'
+    },
+    {
+        firstName: 'Clarke',
+        lastName: 'Schmidt',
+        position: 'P',
+        age: 29,
+        photo: 'imgs/clarke.png'
+    },
+     {
+        firstName: 'Carlos',
+        lastName: 'Rodon',
+        position: 'P',
+        age: 32,
+        photo: 'imgs/rodon.png'
+    },
+    {
+        firstName: 'Mark',
+        lastName: 'Leiter Jr.',
+        position: 'P',
+        age: 34,
+        photo: 'imgs/leiter.png'
+    },
+    {
+        firstName: 'Tim',
+        lastName: 'Hill',
+        position: 'P',
+        age: 35,
+        photo: 'imgs/hill.png'
+    },
+    {
+        firstName: 'Ian',
+        lastName: 'Hamilton',
+        position: 'P',
+        age: 29,
+        photo: 'imgs/hamilton.png'
+    },
+     {
+        firstName: 'Max',
+        lastName: 'Fried',
+        position: 'P',
+        age: 31,
+        photo: 'imgs/fried.png'
+    },
+    {
+        firstName: 'Fernado',
+        lastName: 'Cruz',
+        position: 'P',
+        age: 35,
+        photo: 'imgs/cruz.png'
+    },
+    {
+        firstName: 'Giancarlo',
+        lastName: 'Stanton',
+        position: 'DH',
+        age: 35,
+        photo: 'imgs/stanton.png'
+    },
+    {
+        firstName: 'Gerrit',
+        lastName: 'Cole',
+        position: 'SP',
+        age: 34,
+        photo: 'imgs/cole.png'
+    },
+    {
+        firstName: 'Luis',
+        lastName: 'Gil',
+        position: 'SP',
+        age: 26,
+        photo: 'imgs/gil.png'
+    },
+   
+    
 
 ]
